0b240cd0419043b65a6fdc0c0e0c5e0d91504f9e
Joey Arnstein and paul hess <pauljacksonhess@gmail.com>
2016-02-01 10:03:44 -0800

    add basic properties of Issue object

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Issue(name, location, language, description, snippet) {
+  this.name = name;
+  this.location = location;
+  this.language = language;
+  this.description = description;
+  this.snippet = snippet;
+};
