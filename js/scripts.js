00e438845e5435fee59967a5f4add65e001a3a3e
Brad Pizzimenti, Chris Kuiper, Joey Arnstein and Paul Hess <chris.kuiper@gmail.com>
2016-02-02 17:20:16 -0800

    add <xmp> tag to code snippt to keep it from breaking, css styling of codesnippet area

--- js/scripts.js
+++ js/scripts.js
@@ -59,14 +59,13 @@ $(document).ready(function(){
                   '<div class="modal-header">'+
                     '<button type="button" class="close" data-dismiss="modal">&times;'+
                     '</button>'+
-                    '<h4 class="modal-title">Queue</h4>'+
+                    '<h4 class="modal-title">'+issue.name+'</h4>'+
                   '</div>'+
                   '<div class="modal-body">'+
-                    '<p>'+issue.name+'</p>'+
-                    '<p>'+issue.location+'</p>'+
-                    '<p>'+issue.language+'</p>'+
-                    '<p>'+issue.description+'</p>'+
-                    '<p><xmp>'+issue.snippet+'</xmp></p>'+
+                    '<p>Location:<span class="modal-data">'+issue.location+'</span></p>'+
+                    '<p>Language:<span class="modal-data">'+issue.language+'</span></p>'+
+                    '<p>Description:<span class="modal-data">'+issue.description+'</span></p>'+
+                    '<p>Code Snippet:<span class="modal-data"><xmp>'+issue.snippet+'</xmp></span></p>'+
                   '</div>'+
                 '</div>'+
               '</div>'+
